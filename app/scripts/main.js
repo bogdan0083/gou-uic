3e649b16ffc88738debbd6657a62510c3e28fd4c
Bogdan <d.bogdan@inbox.ru>
2016-12-16 12:56:40 +0700

    progress pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
     // SVG FOR EVERYBODY! (svg polyfill)
     svg4everybody();
 
+    $('.nav-trigger-wrap').on('click', function(e) {
+        e.preventDefault();
+
+        $(this).find('.nav-trigger').toggleClass('active');
+
+        $('.mobile-menu').toggleClass('active');
+        $('.page-wrapper').toggleClass('active');
+    });
+
+    var $megaSlider = $('.mega-slider');
+
+    $megaSlider.find('.wrapper').slick({
+        arrows: false,
+        dots: false
+    });
 });
