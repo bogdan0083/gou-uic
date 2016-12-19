d6c5fc19369b9bb6ad869a96d111d3e2d972363c
Bogdan <d.bogdan@inbox.ru>
2016-12-19 15:34:58 +0700

    progress pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,10 +12,28 @@ $(document).ready(function () {
         $('.page-wrapper').toggleClass('active');
     });
 
-    var $megaSlider = $('.mega-slider');
+    var $megaSlider = $('.mega-slider'),
+        $doubleSlider = $('.double-slider');
 
     $megaSlider.find('.wrapper').slick({
         arrows: false,
         dots: false
+    });
+
+    $doubleSlider.slick({
+        arrows: false,
+        dots: false,
+        adaptiveHeight: true,
+        infinite: false,
+        autoplay: false,
+        fade: true
+    });
+
+    $('.features-and-types .tab').click(function (e) {
+        e.preventDefault();
+        $doubleSlider.slick('slickGoTo', $(this).index());
+
+        $('.features-and-types .tab').removeClass('js-active');
+        $(this).addClass('js-active');
     });
 });
